44eee65bf2241bb7a8a2b88e45e9971efbf0e6f8
xtophe02 <moreira.christophe@outlook.com>
2018-12-29 15:34:24 +0100

    added validation on router register and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load Iput Validation
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 // Load User model
 const User = require('../../models/User')
@@ -17,14 +24,22 @@ router.get('/test', (req, res) => res.json({
 // @description Register user
 // @access      Public
 router.post('/register', (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validateRegisterInput(req.body)
+
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
   User.findOne({
       email: req.body.email
     })
     .then(user => {
       if (user) {
-        return res.status(400).json({
-          email: 'Email already exists'
-        })
+        errors.email = 'Email already exists'
+        return res.status(400).json(errors)
       } else {
         // make avatar
         const avatar = gravatar.url(req.body.email, {
@@ -57,8 +72,20 @@ router.post('/register', (req, res) => {
 // @description Login user / returning jsonWebToken token
 // @access      Public
 router.post('/login', (req, res) => {
+
+  const {
+    errors,
+    isValid
+  } = validateLoginInput(req.body)
+
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
   const email = req.body.email;
   const password = req.body.password;
+
+
 
   //Find user by email
   User.findOne({
@@ -66,26 +93,51 @@ router.post('/login', (req, res) => {
     })
     .then(user => {
       if (!user) {
-        return res.status(404).json({
-          msg: 'User not found'
-        })
+        errors.email = 'User not found';
+        return res.status(404).json(errors)
       }
       // check password
       bcryptjs.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({
-              msg: 'Success'
-            })
+            // User matched
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            } //create JWT payload
+            // Sign the Token
+            jwt.sign(
+              payload,
+              keys.secretOrKey, {
+                expiresIn: 3600
+              },
+              (err, token) => {
+                if (err) throw err;
+                res.json({
+                  success: true,
+                  token: `Bearer ${token}`
+                })
+              });
           } else {
-            return res.status(400).json({
-              password: 'Password incorrect'
-            })
+            errors.password = 'Password incorrect'
+            return res.status(400).json(errors)
           }
         })
     })
 });
 
-module.exports = router; {
-  email: 'User not found'
-}
+// @route       POST api/users/currnt
+// @description Return current user
+// @access      Private
+router.get('/current', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -10,19 +11,24 @@ const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({
-  extended: false,
-  useNewUrlParser: true
+  extended: false
 }));
 app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI
 //Connect to MLAB
-mongoose.connect(db)
+mongoose.connect(db, {
+    useNewUrlParser: true
+  })
   .then(() => console.log('MLAB connected'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('hello'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use routes
 app.use('/api/users', users);
