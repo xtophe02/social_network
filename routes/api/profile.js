07e79b903221448b8c442842bbbf880db77991a1
xtophe02 <moreira.christophe@outlook.com>
2018-12-30 10:42:55 +0100

    added profile schema

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile Model
+const Profile = require('../../models/Profile');
+// Load User Model
+const User = require('../../models/User');
 
 // @route       GET api/profile/test
 // @description Tests profile route
@@ -7,5 +14,23 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
   msg: 'Profile Works'
 }));
+
+// @route       GET api/profile
+// @description Get current users profile
+// @access      Private
+router.get('/', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const errors = {}
+  Profile.findOne({
+      user: req.user.id
+    })
+    .then(profile => {
+      errors.noprofile = 'There is no profile for this user';
+      if (!profile) return res.status(404).json(errors);
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err))
+})
 
 module.exports = router;
